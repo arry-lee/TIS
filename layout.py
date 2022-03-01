17967f050b4bdbc3048f451baaa061d56d548eb1
arry-lee <arry_lee@qq.com>
2022-03-01 17:51:39 +0800

    remove fontwrap as new module

--- layout.py
+++ layout.py
@@ -8,9 +8,10 @@ from abc import ABCMeta, abstractmethod
 import cv2
 import numpy as np
 import prettytable
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 
-from awesometable import AwesomeTable, from_str, hstack, vstack
+from awesometable import AwesomeTable
+from fontwrap import put_text_in_box, put_text_in_box_without_break_word
 from table2image import table2image
 
 
@@ -251,7 +252,8 @@ class FlexTable(AwesomeTable):
         return table2image(str(self),font_size=self.font_size,vrules=None,hrules=None,style='others')
 
 
-# todo 移除 get_image 中的参数
+
+
 class TextBlock(AbstractTable):
     def __init__(self, text, width=70 * 20, indent=0, font_path="arial.ttf",
                  font_size=20, padding=0, **kwargs):
@@ -311,14 +313,15 @@ class TextBlock(AbstractTable):
             s = "\n".join(self.wrap_text)
             return self.double_end_align(s, self._char_width)
         else:
-            return put_text_in_box(self.text, self._table_width)[0]
+            return put_text_in_box(self.text, self._table_width, break_word=False)[0]
 
     def get_image(self):
-        s, img,boxes = put_text_in_box(
+        s, img,boxes = put_text_in_box_without_break_word(
             self.text,
             self._table_width,
             font_size=self.font_size,
             font_path=self.font_path,
+            # break_word=False
         )
 
         bg = Image.new("RGB", (img.width + self.padding*2, img.height + self.padding*2),
@@ -338,155 +341,18 @@ class TextBlock(AbstractTable):
         }
 
 
-def draw_multiline_text(xy, text, fill, font_path="arial.ttf", font_size=20):
-    """通过像素偏移实现两端对齐"""
-    text_list = text.splitlines()
-    font = ImageFont.truetype(font_path, font_size)
-    width = max(font.getsize(t)[0] for t in text_list) + font_size * 5
-    img = Image.new("RGB", (width, font_size * len(text_list)), "white")
-    draw = ImageDraw.Draw(img)
-    x, y = xy
-    max_width = 0
-    for t in text_list[:-1]:
-        w = font.getsize(t)[0]
-        pix = width - w
-        chars = len(t) - 1
-        n = pix // chars
-        m = pix % chars
-        offsets = [n] * chars
-        for i in range(m):
-            offsets[i] += 1
-        offsets.append(0)
-
-        for c, b in zip(t, offsets):
-            draw.text((x, y), c, fill, font)
-            w = font.getsize(c)[0]
-            x += w + b
-        max_width = max(max_width, x)
-        x = 0
-        y += font_size
-    draw.text((x, y), text_list[-1], fill, font)
-    height = y + font_size
-    # img = img.crop((0,0,max_width,height))
-    img.show()
-    return img
-
-
-def put_text_in_box(
-        text, width, fill="black", font_path="arial.ttf", font_size=20,
-        line_pad=4
-):
-    # 不硬换行，软换行
-    font = ImageFont.truetype(font_path, font_size)
-    img = Image.new("RGB", (width, width), "white")
-    draw = ImageDraw.Draw(img)
-
-    space_width = font.getsize(" ")[0]
-    indent = 0
-    for i in text:
-        if i.isspace():
-            indent += 1
-        else:
-            break
-    words = text.split()
-    # words[0] = " " * indent + words[0]
-    lines = []
-    line = ""
-    boxes = []
-    x, y = space_width*indent, 0
-    x0 = x
-    for word in words:
-        bbox = draw.textbbox((x, y), word, font)
-        if bbox[2] < width:
-            draw.text((x, y), word + " ", fill, font)
-            line += word + " "
-            x = bbox[2] + space_width
-        elif bbox[2] > width:
-            for i, c in enumerate(word):
-                bbox = draw.textbbox((x, y), c, font)
-                if bbox[2] < width - space_width:
-                    draw.text((x, y), c, fill, font)
-                    line += c
-                    x = bbox[2]
-                else:
-                    if i > 0:
-                        draw.text((x, y), "-", fill, font)
-                        line += "-"
-                    lines.append(line)
-                    boxes.append((x0,y,width,y+font_size))
-                    x = 0
-                    x0 = 0
-                    y += font_size + line_pad
-                    line = word[i:] + " "
-                    draw.text((x, y), line, fill, font)
-                    x = draw.textbbox((x, y), line, font)[2]
-                    break
-        else:
-            draw.text((x, y), word, fill, font)
-            line += word
-            lines.append(line)
-            boxes.append((0, y, width, y + font_size))
-            line = ""
-            x = 0
-            y += font_size + line_pad
-
-    lines.append(line)
-    bbox = draw.textbbox((0, y), line, font)
-    boxes.append(bbox)
-    height = y + font_size + line_pad
-    img = img.crop((0, 0, width, height))
-    # points = []
-    # for box in boxes:
-    #     points.append([box[0], box[1]])
-    #     points.append([box[2], box[1]])
-    #     points.append([box[2], box[3]])
-    #     points.append([box[0], box[3]])
-    # label = ["text@"+l for l in lines]
-
-    return "\n".join(lines), img,boxes
-
-
-def from_list(ls, t2b=True, w=None):
-    if t2b:
-        rows = []
-        for value in ls:
-            if isinstance(value, list):
-                tv = from_list(value, not t2b, w)
-            elif isinstance(value, AwesomeTable):
-                value.table_width = w
-                tv = AwesomeTable()
-                tv.add_row([value.get_string()])
-            else:
-                tv = from_str(value, w + 2)
-
-            rows.append(tv)
-        return vstack(rows)
-    else:
-        cols = []
-        for value in ls:
-            if isinstance(value, list):
-                tv = from_list(value, not t2b, w)
-            elif isinstance(value, AwesomeTable):
-                value.table_width = w
-                tv = AwesomeTable()
-                tv.add_row([value.get_string()])
-            else:
-                tv = from_str(value, w + 2)
-            cols.append(tv)
-        return hstack(cols)
-
-
 if __name__ == "__main__":
     import faker
 
-    # f = faker.Faker()
-    # t = TextBlock(f.paragraph(10), 38, 4)
+    f = faker.Faker()
+    # t = TextBlock(f.paragraph(10), 400, 0)
+    #
     # s = t.get_string()
+
+    img = put_text_in_box_without_break_word(f.paragraph(10), 800, font_size=20)
+
     # print(s)
+    # x = draw_multiline_text(s,'black')
+    img.save('x.jpg')
     # img = t.get_image()["image"]
-    # img.show()
-    a = FlexTable(w=1000,font_size=40)
-    a.add_rows([[12,34,56],[33,44,55]])
-    x = a.get_image()['image']
-    cv2.imwrite('t.jpg',x)
-    print(x.shape)
+    # cv2.imwrite('t.jpg',img)
