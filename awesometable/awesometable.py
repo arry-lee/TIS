58fef3eb782ad69ec669c8538390cdd98830cc8a
arry-lee <arry_lee@qq.com>
2022-12-11 20:00:09 +0800

    modify awesometable.py

--- awesometable/awesometable.py
+++ awesometable/awesometable.py
@@ -2,14 +2,11 @@
 awesometable create text table in simple way
 """
 import re
-import textwrap
-from functools import reduce
+from functools import partial, reduce
 
 import prettytable
 import wcwidth
 from prettytable import ALL, FRAME
-
-from utils.ulpb import is_chinese
 
 V_LINE_PATTERN = re.compile("[║╬╦╩╣╠╗╔╝╚]")
 H_LINE_PATTERN = re.compile(r"(\n*[╔╠╚].+?[╗╣╝]\n*)")
@@ -28,6 +25,7 @@ def get_size(text):
     width = max(str_block_width(line) for line in lines)
     return width, height
 
+
 def remove_invisible_chars(chars):
     """移除所有不可见字符，除\n外"""
     text = ''
@@ -38,17 +36,20 @@ def remove_invisible_chars(chars):
             text += char
     return text
 
+
 def remove_tone_chars(chars):
     text = ''
     for char in chars:
-        if wcwidth.wcswidth(_re.sub("", char))==0:
+        if wcwidth.wcswidth(_re.sub("", char)) == 0:
             text += ''
         else:
             text += char
     return text
 
+
 def clean_chars(chars):
     return remove_invisible_chars(remove_tone_chars(chars))
+
 
 def str_block_width(val):
     """文本形式宽度"""
@@ -65,7 +66,7 @@ class AwesomeTable(prettytable.PrettyTable):
     title_pos   标题所在位置
     max_cols    最大列数
     """
-
+    
     def __init__(self, rows=None, field_names=None, **kwargs):
         super().__init__(field_names, kwargs=kwargs)
         if rows:
@@ -76,7 +77,7 @@ class AwesomeTable(prettytable.PrettyTable):
         self._title_pos = kwargs.get("title_pos", "t")
         self._title = kwargs.get("title", None)
         self.wrap_func = kwargs.get("wrap_func", wrap)  # 为了向后兼容使用不同字体
-
+    
     def __getitem__(self, index):
         new = AwesomeTable()
         new.field_names = self.field_names
@@ -89,14 +90,15 @@ class AwesomeTable(prettytable.PrettyTable):
         elif isinstance(index, int):
             new.add_row(self._rows[index])
         else:
-            raise Exception(f"Index {index} is invalid, must be an integer or slice")
+            raise Exception(
+                f"Index {index} is invalid, must be an integer or slice")
         return new
-
+    
     @property
     def title_pos(self):
         """标题位置"""
         return self._title_pos
-
+    
     @title_pos.setter
     def title_pos(self, pos="t"):
         """在表格的前后左右加标题框"""
@@ -104,54 +106,54 @@ class AwesomeTable(prettytable.PrettyTable):
             self._title_pos = pos
         else:
             raise KeyError("%s not in lrtb")
-
+    
     @property
     def table_width(self):
         """字符表格宽度"""
         return len(str(self.get_string().split("\n")[0]))
-
+    
     @table_width.setter
     def table_width(self, val):
         self._validate_option("table_width", val)
         self._min_table_width = val
         self._table_width = val
         self._max_table_width = val
-
+    
     @property
     def table_height(self):
         """字符表格高度"""
         return len(self.get_string().split("\n"))
-
+    
     @property
     def widths(self):
         """字符表格列宽"""
         return self._widths
-
+    
     @widths.setter
     def widths(self, val):
         self._widths = val
-
+    
     def get_string(self, **kwargs):
         """字符串表示"""
         options = self._get_options(kwargs)
         title = options["title"] or self._title
         if not title:
             return super().get_string(header=False)
-
+        
         title_pos = self._title_pos
         if title_pos == "t":
             return super().get_string(header=False)
-
+        
         self._title = False  # 无标题字符串
         sos = super().get_string(header=False)
         self._title = title  # 代码位置很关键
-
+        
         lines = sos.splitlines()
         if title_pos == "b":
             tab = self._stringify_title(title, options)
             tab = tab + "\n" + "╚" + "═" * (len(lines[0]) - 2) + "╝"
             return _vstack(sos, tab)
-
+        
         height = len(lines) - 2  # 实际可用的高度
         width, _ = get_size(title)
         vtitle = self._stringify_vertical_title(
@@ -161,19 +163,19 @@ class AwesomeTable(prettytable.PrettyTable):
             return _hstack(vtitle, sos)
         if title_pos == "r":
             return _hstack(sos, vtitle)
-
+    
     def _compute_table_width(self, options):
         """增加了竖线数量的统计"""
         table_width = 2 if options["vrules"] in (FRAME, ALL) else 0
         per_col_padding = sum(self._get_padding_widths(options))
         for index, fieldname in enumerate(self.field_names):
             if not options["fields"] or (
-                options["fields"] and fieldname in options["fields"]
+                    options["fields"] and fieldname in options["fields"]
             ):
                 table_width += self._widths[index] + per_col_padding
         table_width += len(self._widths) - 1  #
         return table_width
-
+    
     def _compute_widths(self, rows, options):
         """父类中成倍放大宽度并不精确，此处改为逐个增加宽度"""
         if self._widths:
@@ -193,9 +195,10 @@ class AwesomeTable(prettytable.PrettyTable):
                 else:
                     widths[index] = max(widths[index], get_size(value)[0])
                 if fieldname in self.min_width:
-                    widths[index] = max(widths[index], self.min_width[fieldname])
+                    widths[index] = max(widths[index],
+                                        self.min_width[fieldname])
         self._widths = widths
-
+        
         if self._max_table_width:
             table_width = self._compute_table_width(options)
             if table_width > self._max_table_width:
@@ -223,8 +226,9 @@ class AwesomeTable(prettytable.PrettyTable):
                 for i in range(num % len(widths)):
                     widths[i] += 1
                 self._widths = widths
-
-    def _stringify_vertical_title(self, title, width, height, options, valign="m"):
+    
+    def _stringify_vertical_title(self, title, width, height, options,
+                                  valign="m"):
         lines = title.split("\n")  # 可以包含换行
         new_lines = []
         for line in lines:
@@ -233,9 +237,9 @@ class AwesomeTable(prettytable.PrettyTable):
             new_lines.append(line)
         lines = new_lines
         value = "\n".join(lines)
-
+        
         row_height = height
-
+        
         bits = []
         lpad, rpad = self._get_padding_widths(options)
         for i in range(0, row_height):
@@ -245,16 +249,16 @@ class AwesomeTable(prettytable.PrettyTable):
                     bits[i].append(self.vertical_char)
                 else:
                     bits[i].append(" ")
-
+        
         lines = value.split("\n")
         d_height = row_height - len(lines)  # 空行的高度
-
+        
         if d_height:
             if valign == "m":  # 居中
                 lines = (
-                    [""] * int(d_height / 2)
-                    + lines
-                    + [""] * (d_height - int(d_height / 2))
+                        [""] * int(d_height / 2)
+                        + lines
+                        + [""] * (d_height - int(d_height / 2))
                 )
             elif valign == "b":
                 lines = [""] * d_height + lines
@@ -262,40 +266,41 @@ class AwesomeTable(prettytable.PrettyTable):
                 lines = lines + [" " * width] * d_height
         i = 0
         for line in lines:
-            bits[i].append(" " * lpad + self._justify(line, width, "m") + " " * rpad)
+            bits[i].append(
+                " " * lpad + self._justify(line, width, "m") + " " * rpad)
             if options["border"]:
                 if options["vrules"] == ALL:
                     bits[i].append(self.vertical_char)
                 else:
                     bits[i].append(" ")
             i += 1
-
+        
         for i in range(0, row_height):
             if options["border"] and options["vrules"] == FRAME:
                 bits[i].pop()
                 bits[i].append(options["vertical_char"])
-
+        
         for i in range(0, row_height):
             bits[i] = "".join(bits[i])
-
+        
         first_line = [options["top_left_junction_char"]]
         first_line.extend([options["horizontal_char"]] * (width + 2))
         first_line.append(options["top_right_junction_char"])
         first_line = "".join(first_line)
-
+        
         end_line = [options["bottom_left_junction_char"]]
         end_line.extend([options["horizontal_char"]] * (width + 2))
         end_line.append(options["bottom_right_junction_char"])
         end_line = "".join(end_line)
-
+        
         bits.insert(0, first_line)
         bits.append(end_line)
-
+        
         return "\n".join(bits)
-
+    
     def _stringify_row(self, row, options, hrule):
         for (index, field, value, width) in zip(
-            range(0, len(row)), self._field_names, row, self._widths
+                range(0, len(row)), self._field_names, row, self._widths
         ):
             # Enforce max widths
             lines = value.split("\n")
@@ -307,13 +312,13 @@ class AwesomeTable(prettytable.PrettyTable):
             lines = new_lines
             value = "\n".join(lines)
             row[index] = value
-
+        
         row_height = 0
         for one in row:
             height = get_size(one)[1]
             if height > row_height:
                 row_height = height
-
+        
         bits = []
         lpad, rpad = self._get_padding_widths(options)
         for i in range(0, row_height):
@@ -323,7 +328,7 @@ class AwesomeTable(prettytable.PrettyTable):
                     bits[i].append(self.vertical_char)
                 else:
                     bits[i].append(" ")
-
+        
         for (field, value, width) in zip(self._field_names, row, self._widths):
             valign = self._valign[field]
             lines = value.split("\n")
@@ -331,9 +336,9 @@ class AwesomeTable(prettytable.PrettyTable):
             if d_height:
                 if valign == "m":
                     lines = (
-                        [""] * int(d_height / 2)
-                        + lines
-                        + [""] * (d_height - int(d_height / 2))
+                            [""] * int(d_height / 2)
+                            + lines
+                            + [""] * (d_height - int(d_height / 2))
                     )
                 elif valign == "b":
                     lines = [""] * d_height + lines
@@ -360,55 +365,55 @@ class AwesomeTable(prettytable.PrettyTable):
             if options["border"] and options["vrules"] == FRAME:
                 bits[i].pop()
                 bits[i].append(options["vertical_char"])
-
+        
         if options["border"] and options["hrules"] == ALL:
             bits[row_height - 1].append("\n")
             bits[row_height - 1].append(hrule)
-
+        
         for i in range(0, row_height):
             bits[i] = "".join(bits[i])
-
+        
         return "\n".join(bits)
-
+    
     # def get_image(self, **kwargs):
     #     return table2image(str(self), **kwargs)
-
+    
     def __str__(self):
         return self.get_string()
-
+    
     def __add__(self, other):
         return hstack(self, other)
 
 
 class MultiTable(object):
     """多表格类"""
-
+    
     def __init__(self, tables):
         self._tables = tables
         self._table_width = 0
-
+    
     def add_table(self, table):
         """增加表格"""
         assert isinstance(table, AwesomeTable)
         self._tables.append(table)
-
+    
     @property
     def table_width(self):
         """表格宽度"""
         return self._table_width
-
+    
     @table_width.setter
     def table_width(self, val):
         self._table_width = val
         for table in self._tables:
             table.table_width = val
-
+    
     def get_string(self):
         """多表字符串"""
         if self._table_width == 0:
             self.table_width = max([t.table_width for t in self._tables])
         return vstack(self._tables)
-
+    
     def __str__(self):
         return self.get_string()
 
@@ -493,7 +498,7 @@ def set_align(table, cno, align, rno=None):
                 line = line.replace(cell, new_cell)
                 out.append(line)
         return "\n".join(out)
-
+    
     line = lines[2 * rno + 1]
     cells = re.split(V_LINE_PATTERN, line)[1:-1]
     cell = cells[cno]
@@ -535,7 +540,7 @@ def remove_hor_line(table, start=2, end=-1, keep_blank=True):
     return "\n".join(lines)
 
 
-def add_width(table, num=1):
+def add_width(table, num=1,align='m'):
     """
     增加表格宽度
     :param table: AwesomeTable | str
@@ -551,14 +556,33 @@ def add_width(table, num=1):
                 newline = line[0] + line[1:-1] + line[-2] * num + line[-1]
             elif "║" in line:
                 cells = line.rsplit("║", 2)
-                newline = (
-                    cells[0]
-                    + "║"
-                    + " " * (num // 2)
-                    + cells[1]
-                    + " " * (num - num // 2)
-                    + "║"
-                )
+                if align=='m':
+                    newline = (
+                            cells[0]
+                            + "║"
+                            + " " * (num // 2)
+                            + cells[1]
+                            + " " * (num - num // 2)
+                            + "║"
+                    )
+                elif align=='l':
+                    newline = (
+                            cells[0]
+                            + "║"
+                            + cells[1]
+                            + " " * num
+                            + "║"
+                    )
+                elif align=='r':
+                    newline = (
+                            cells[0]
+                            + "║"
+                            + " " * num
+                            + cells[1]
+                            + "║"
+                    )
+                else:
+                    raise ValueError('align in l m r')
             else:
                 newline = line
         except IndexError:
@@ -622,30 +646,31 @@ def _hstack(self, other, merged=True, space=0, align="t"):
     other_lines = soo.splitlines()
     new_lines = []
     for left, right in zip(self_lines, other_lines):
-        eol = left[-1]
-        bor = right[0]
-        if eol == bor == "║":
-            ret = "║"
-        elif eol == "╗" and bor == "╔":
-            ret = "╦"
-        elif eol == "╣" and bor == "║":
-            ret = "╣"
-        elif eol == "╣" and bor == "╠":
-            ret = "╬"
-        elif eol == "║" and bor == "╠":
-            ret = "╠"
-        elif eol == "╝" and bor == "╚":
-            ret = "╩"
-        else:
-            raise Exception("Unmatch symbol %s and %s" % (eol, bor))
         if merged:
+            eol = left[-1]
+            bor = right[0]
+            if eol == bor == "║":
+                ret = "║"
+            elif eol == "╗" and bor == "╔":
+                ret = "╦"
+            elif eol == "╣" and bor == "║":
+                ret = "╣"
+            elif eol == "╣" and bor == "╠":
+                ret = "╬"
+            elif eol == "║" and bor == "╠":
+                ret = "╠"
+            elif eol == "╝" and bor == "╚":
+                ret = "╩"
+            else:
+                print(other)
+                raise Exception("Unmatch symbol %s and %s" % (eol, bor))
             new_lines.append(left[:-1] + ret + right[1:])
         else:
             new_lines.append(left + " " * space + right)
     return "\n".join(new_lines)
 
 
-def _vstack(self, other, merged=True):
+def _vstack(self, other, merged=True,align='m'):
     """
     表格字符纵向拼接
     :param self: AwesomeTable | str 左侧
@@ -658,16 +683,16 @@ def _vstack(self, other, merged=True):
     hos = len(sos.splitlines()[0])
     hoo = len(soo.splitlines()[0])
     if hos > hoo:
-        soo = add_width(soo, hos - hoo)
+        soo = add_width(soo, hos - hoo,align)
     elif hos < hoo:
-        sos = add_width(sos, hoo - hos)
+        sos = add_width(sos, hoo - hos,align)
     else:
         pass
     if sos.splitlines()[-1][-1] != "╝" or soo[0] != "╔":
         return sos + "\n" + soo
     if not merged:
-        return sos + "\n\n" + soo
-
+        return sos + "\n" + soo#{' '*len(sos.splitlines()[0])}
+    
     end_of_self = sos.splitlines()[-1]
     begin_of_other = soo.splitlines()[0]
     ret = []
@@ -693,7 +718,7 @@ def _vstack(self, other, merged=True):
     return "\n".join(new_lines)
 
 
-def hstack(tables, other=None):
+def hstack(tables, other=None,merged=True,align='t'):
     """
     横向连接多个表格
     :param tables: list[AwesomeTable] | AwesomeTable
@@ -701,11 +726,11 @@ def hstack(tables, other=None):
     :return: str
     """
     if isinstance(tables, list) and other is None:
-        return reduce(_hstack, tables)
-    return _hstack(tables, other)
+        return reduce(partial(_hstack,merged=merged,align=align), tables)
+    return _hstack(tables, other,merged,align)
 
 
-def vstack(tables, other=None):
+def vstack(tables, other=None,merged=True,align='m'):
     """
     纵向连接多个表格
     :param tables: list[AwesomeTable] | AwesomeTable
@@ -713,8 +738,8 @@ def vstack(tables, other=None):
     :return: str
     """
     if isinstance(tables, list) and other is None:
-        return reduce(_vstack, tables)
-    return _vstack(tables, other)
+        return reduce(partial(_vstack,merged=merged,align=align), tables)
+    return _vstack(tables, other,merged,align)
 
 
 def stack(tables):
@@ -738,7 +763,7 @@ def wrap(line, width):
     lines = []
     new_line = ""
     for char in line:
-        if not char.isprintable(): # 忽略不可见字符
+        if not char.isprintable():  # 忽略不可见字符
             continue
         new_line += char
         if str_block_width(new_line) == width:
